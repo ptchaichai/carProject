7959f45e29bd98e8fa172be32f31fefa3581f17c
cmm <cmmsuju@163.com>
2019-04-26 15:19:05 +0800

    封装分页函数

--- admin/app/routes.js
+++ admin/app/routes.js
@@ -114,88 +114,50 @@ module.exports = function(app, passport) {
 		let id = req.session.passport.user || userInfo.id;
 		if(req.body) {
 			let param = req.body; //获取的参数
-			let sql = '';
+			let sql = null;
 			let role = req.body.role || userInfo.role;
 			let optionParams = [];
+			
 			if(param.page == -1) {
 				optionParams = [];
 				sql = role == 0 ? "SELECT username, phone, role, birthday, time, sex  FROM user" : "SELECT username, phone, role, birthday, time, sex FROM user where role = 2";
-				connection.query(sql, function(err, rows){
-					if(err) {
-						res.json({
-							status: 1,
-							data: err
-						})
-					} else {
-						res.json({
-							status: 0,
-							data: rows
-						})
-					}
-				});
+				findAll(sql, res);
 			} else {
 			  let page= parseInt(param.page || 1); //页码
 			  let end = parseInt(param.pageSize || 10); //页数
 				let start = (page - 1) * end
 				let search = {
-					name: page.search_idx,
-					value: page.search_value
+					name: param.search_idx,
+					value: param.search_value
 				};
-				//为了提高性能，就不放到一个sql语句了,并且不进行封装
-				let countSql = role == 1 ? "SELECT COUNT(*) FROM  user where role = 2" : "SELECT COUNT(*) FROM  user"
-				let totalCount = 0;
-				connection.query(countSql, function(err, count){
-					if(err) {
-						res.json({
-							status: 1,
-							data: err
-						})
-					} else {
-						totalCount = count;
-						console.log(count)
-						if(role == 1) {
-							if(search.name == 'username') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and username like ?" : "SELECT *  FROM user ORDER BY time desc limit ?, ? where role = 2" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							} else if(search.name == 'sex') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and sex like ?" : "SELECT * FROM user order by time desc limit ?, ? where role = 2" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							} else if(search.name == 'phone') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and phone like ?" : "SELECT * FROM user order by time desc limit ?, ? where role = 2" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							}
-						} else if(role == 0) {
-							if(search.name == 'username') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where username like ?" : "SELECT *  FROM user order by time desc limit ?, ?" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							} else if(search.name == 'sex') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where sex like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							} else if(search.name == 'phone') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where phone like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							} else if(search.name == 'role') {
-								sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
-								optionParams = search.value ? [start, end, `%${search.value}%`] : [start, end]
-							}
-						}
-					}
-					connection.query(sql, optionParams, function(err, rows){
-						if(err) {
-							res.json({
-								status: 1,
-								data: err
-							})
-						} else {
-							res.json({
-								status: 0,
-								data: rows,
-								count: totalCount,
-								current_page: page
-							})
-						}
-					})
-				});
+				console.log(search.value)
+				//为了提高性能，就不放到一个sql语句了
+				let countSql = role == 1 ? "SELECT COUNT(*) FROM  user where role = 2 and pass" : "SELECT COUNT(*) FROM  user"
+				// if(role == 1) {
+				// 	if(search.name == 'username') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and username like ?" : "SELECT *  FROM user ORDER BY time desc limit ?, ? where role = 2" 
+				// 	} else if(search.name == 'sex') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and sex like ?" : "SELECT * FROM user order by time desc limit ?, ? where role = 2" 
+				// 	} else if(search.name == 'phone') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role = 2 and phone like ?" : "SELECT * FROM user order by time desc limit ?, ? where role = 2" 
+				// 	}
+				// } else if(role == 0) {
+				// 	if(search.name == 'username') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where username like ?" : "SELECT *  FROM user order by time desc limit ?, ?" 
+				// 	} else if(search.name == 'sex') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where sex like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
+				// 	} else if(search.name == 'phone') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where phone like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
+				// 	} else if(search.name == 'role') {
+				// 		sql = search.value ? "SELECT * FROM user order by time desc limit ?, ? where role like ?" : "SELECT * FROM user order by time desc limit ?, ?" 
+				// 	}
+				// }
+
+				sql = {
+					count: countSql,
+					page: `SELECT * FROM user WHERE username LIKE '%${search.value}%' ORDER BY time desc limit ${start}, ${end}`
+				}
+				pageNation(param, sql, res)
 			}
 		}
 	})
@@ -208,12 +170,69 @@ module.exports = function(app, passport) {
 
 // 判断是否登录的中间件
 function isLoggedIn(req, res, next) {
-	console.log('session', req.session)
-	console.log('isAuthenticated', req.isAuthenticated())
+	// console.log('session', req.session)
+	// console.log('isAuthenticated', req.isAuthenticated())
 	if (req.isAuthenticated()) return next();
 	res.json({
 		status: 2,
 		data: '没有登录'
 	})
 }
+
+/**
+ * 获取列表所有数据
+ * @param {*} sql 
+ */
+	function findAll(sql, res) {
+		connection.query(sql, function(err, rows){
+			if(err) {
+				res.json({
+					status: 1,
+					data: err
+				})
+				return
+			} else {
+				res.json({
+					status: 0,
+					data: rows
+				})
+			}
+		});
+	}
+  /**
+	 * 分页函数封装
+	 * @param 
+	 */
+	function pageNation(param, sql, res){
+		let page= parseInt(param.page || 1); //页码
+		let pageSize = parseInt(param.pageSize || 10); //页数
+		connection.query(sql.count, function(err, count){
+			if(err) {
+				res.json({
+					status: 1,
+					data: err
+				})
+				return
+			} else {
+				let totalCount = JSON.parse(JSON.stringify(count[0])) || 0;
+				connection.query(sql.page, function(err, rows){
+					if(err) {
+						res.json({
+							status: 1,
+							data: err
+						})
+						return
+					} else {
+						res.json({
+							status: 0,
+							count: totalCount['COUNT(*)'],
+							current_page: page,
+							total_page: Math.ceil(totalCount['COUNT(*)'] / pageSize),
+							data: rows
+						})
+					}
+				})
+			}
+		})
+	}
 }
