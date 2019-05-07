fab6617e5f33f3ad15d3f23d69c18d41c3883d8f
cmm <cmmsuju@163.com>
2019-05-07 16:29:38 +0800

    修改数据库密码

--- admin/config/database.js
+++ admin/config/database.js
@@ -3,8 +3,8 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': '123456'
-
+        //'password': '123456'
+        'password': 'tt123456'
     },
 	'database': 'car',
     'users_table': 'car'
